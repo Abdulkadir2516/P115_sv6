8d6161dca59d77c2a167935fee13f03afa43b57e
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2024-03-30 21:45:50 +0300

    Create 2_exp_for_loop.py

---
+++ 4_Hafta/2_exp_for_loop.py
@@ -0,0 +1,19 @@
+# Faktöriyel
+import math
+
+import math
+sayi = int(input("Bir Tam Sayı Giriniz: "))
+# print(math.factorial((sayi)))
+
+result = 1
+for i in range(1, sayi+1):
+    result *= i
+
+print(result)
+
+
+
+
+
+
+
