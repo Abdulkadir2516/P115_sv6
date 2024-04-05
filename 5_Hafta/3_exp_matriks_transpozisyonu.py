4e0dcd664784624c9efe903bd21f90aee0cc84b6
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2024-04-05 22:05:07 +0300

    Update 3_exp_matriks_transpozisyonu.py

--- 5_Hafta/3_exp_matriks_transpozisyonu.py
+++ 5_Hafta/3_exp_matriks_transpozisyonu.py
@@ -16,3 +16,25 @@ for i in range(1,26):
 # [16, 17, 18, 19, 20],
 # [21, 22, 23, 24, 25]
 # ]
+
+dizi_t = []
+
+for i in range(len(dizi)):
+    k = []
+    for j in range(5):
+        print(dizi[j][i], end=" " )
+        k.append(dizi[j][i])
+    dizi_t.append(k)
+    print()
+
+print(dizi_t)
+
+#Yöntem 2
+
+dizi = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15]
+]
+sutunlar = [[dizi[a][i] for a in range(len(dizi))] for i in range(len(dizi[0]))]
+print("tranpos = "+str([i for i in sutunlar ]))
