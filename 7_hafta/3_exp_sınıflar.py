e11ba07f74fee5f8f2b12b1c028507d49beab03f
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2024-04-26 20:49:43 +0300

    Create 3_exp_sınıflar.py

---
+++ 7_hafta/3_exp_sınıflar.py
@@ -0,0 +1,24 @@
+
+class Ogrenci:
+
+    def __init__(self):
+        self.adi="Abdulkadir"
+        soyadi = "Yeşilkaya"
+        sinifi = "4.sınıf"
+        okul_no="202113709083"
+        okul_adi="Balıkesir Üniversitesi"
+        yer = "Balıkesir"
+
+    def deneme(self):
+        a = self.adi
+
+
+deneme = Ogrenci()
+
+print(type(deneme))
+
+
+
+
+
+
